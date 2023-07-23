c72e7ed75a9c94623feb968529e675999e33ea07
unknown <isaiaspadros2@gmail.com>
2023-07-23 00:28:23 -0300

    Finalizado de punto 2

---
+++ TpReact2/src/components/Titulo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Titulo = ({subtituloNuevo}) => {
+    return (
+        <>
+        
+        <h1> Hello {subtituloNuevo}</h1>
+        
+        
+        </>
+    );
+};
+
+export default Titulo;
